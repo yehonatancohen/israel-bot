497725f3e457e7ec3b20a20a87fb5436485bbe25
Yehonatan Cohen <coheyeho@gmail.com>
2021-01-08 17:07:29 +0200

    Added slowmode function

--- bot.py
+++ bot.py
@@ -95,9 +95,10 @@ async def help(ctx):
         channel = "לא מחובר לאף חדר"
     await ctx.channel.send(f"צריך את עזרתכם {ctx.message.author.mention} ,{staff.mention}\n{channel} הוא")
 
-@bot.command()
+@c.command()
 @commands.has_permissions(administrator = True)
 async def setdelay(ctx, seconds: int):
+    await ctx.message.delete()
     await ctx.channel.edit(slowmode_delay=seconds)
     await ctx.send(f"שיניתי את הקולדאון ל{seconds} שניות",delete_after=3)
 
